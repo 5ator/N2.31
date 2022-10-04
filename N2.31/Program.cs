2198904fb30c4c9112755e06115756582860be73
User <User@DESKTOP-1QGR0CC>
2022-10-04 22:16:08 +0300

    Добавьте файлы проекта.

---
+++ N2.31/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Mainclass
+{
+    public static void Main()
+    {
+        int n;
+        Console.WriteLine("Введите число от 100 до 999:");
+        n = int.Parse(Console.ReadLine());
+        string n1 = n.ToString();
+        if (n <= 999 && n >= 100)
+        {
+            string a = n1.Remove(1, 2);
+            string b = n1.Remove(0, 1);
+            b = b.Remove(1, 1);
+            string c = n1.Remove(0, 2);
+            string[] many = { a, c, b };
+            string x = string.Join(null, many);
+            Console.WriteLine($"Изначальное число х равно {x}");
+        }
+        else
+        {
+            Console.WriteLine("Введеное число неверно");
+        }
+
+
+
+    }
+}
